60e6d57b5b8bce2603e78618a2fec7d38889f7a4
Ribbined <157750559+CorpseCMD@users.noreply.github.com>
2025-03-06 10:12:12 -0600

    Create FileSystemModule.lua

---
+++ FileSystemModule.lua
@@ -0,0 +1,49 @@
+-- to anyone reading this, I only really made this for me, if you really want you can use it but maybe credit me
+-- bye bye now :)
+
+local API = {}
+local HttpService = game:GetService("HttpService")
+
+local currentFolder = nil
+
+if not isfolder(currentFolder) then
+	makefolder(currentFolder)
+end
+
+function API.SetFolder(folderName)
+	if not isfolder(folderName) then
+		makefolder(folderName)
+	end
+	currentFolder = folderName
+end
+
+function API.GetFileAssetID(fileName)
+	local filePath = currentFolder .. "\\" .. fileName
+	if isfile(filePath) then
+		return getcustomasset(filePath)
+	else
+		return nil, "File does not exist!"
+	end
+end
+
+function API.WriteFile(fileName, content)
+	local filePath = currentFolder .. "\\" .. fileName
+	if not isfile(filePath) then
+		writefile(filePath, content)
+	end
+end
+
+function API.FileExists(fileName)
+	return isfile(currentFolder .. "\\" .. fileName)
+end
+
+function API.ReadFile(fileName)
+	local filePath = currentFolder .. "\\" .. fileName
+	if isfile(filePath) then
+		return readfile(filePath)
+	else
+		return nil, "File does not exist!"
+	end
+end
+
+return API
