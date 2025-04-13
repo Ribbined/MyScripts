987214d0799298aa8e85b904cb2f981817d906e7
Ribbined <157750559+CorpseCMD@users.noreply.github.com>
2025-04-13 12:57:30 -0500

    Create rayfieldLogoRemover.lua

---
+++ rayfieldLogoRemover.lua
@@ -0,0 +1,10 @@
+-- probably won't work for Gen2 but ok
+-- uh i only made this cuz someone wanted it, nothing against rayfield
+local parent = gethui ~= nil and gethui() or game:FindService("CoreGui") or game:GetService("CoreGui") or game:FindFirstChild("CoreGui")
+local function check(c)
+    if c.Name == "Rayfield" and c:FindFirstChild("Loading") then
+        c.Loading.Banner.ImageTransparency = 1
+    end
+end
+parent.ChildAdded:Connect(check)
+for _,c in parent do check(c) end
