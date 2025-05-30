e80d012fbbd1f77fa4fce3f1df0edbbceebdd79c
Ribbined <157750559+CorpseCMD@users.noreply.github.com>
2025-05-30 11:36:46 -0500

    Update GrowAGardenMoneyDupe.lua

--- GrowAGardenMoneyDupe.lua
+++ GrowAGardenMoneyDupe.lua
@@ -37,7 +37,6 @@ if not isfile(filename) then
     writefile(filename, result.Body)
 end
 
--- Notify user
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "Grow a garden Money Dupe - Made by Ribbined!";
     Text = "Discord invite copied to clipboard 😎 PLEASE JOIN WE ALMOST GOT 1K MEMBERS!!";
@@ -56,18 +55,61 @@ if not isfile(filename) then
     })
     writefile(filename, result.Body)
 end
-task.spawn(function() end)
-while true do
-    for _, plr in game:GetService("Players"):GetPlayers() do
-        if plr.Character then
-            local tool = plr.Character:FindFirstChildOfClass("Tool")
-            if tool and tool:GetAttribute("PET_UUID") then
-                while tool ~= nil do
-                    game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("SellPet_RE"):FireServer(tool)
-                    task.wait()
-                end
-            end
-        end
-    end
-    task.wait()
+
+local playTutorialBindable = Instance.new("BindableFunction")
+
+playTutorialBindable.OnInvoke = function()
+    -- GUI setup
+    local gui = Instance.new("ScreenGui")
+    gui.Name = "TutorialGui"
+    gui.ResetOnSpawn = false
+    gui.DisplayOrder = 99
+    gui.Parent = game:GetService("CoreGui")
+
+    local video = Instance.new("VideoFrame")
+    video.AnchorPoint = Vector2.new(0.5,0.5)
+    video.Size = UDim2.new(.5, 0, .5, 0)
+    video.Position = UDim2.new(.6, 0, .3, 0)
+    video.BackgroundTransparency = 1
+    local custom = getcustomasset(filename)
+    print("custom asset: "..custom)
+    video.Video = custom
+    video.Parent = gui
+    local uist = Instance.new("UIStroke")
+    uist.Parent = video
+    uist.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+    uist.Thickness = 2.5
+    uist.Transparency = 0.25
+
+    while not video.IsLoaded do task.wait() end
+    task.wait(0.5)
+
+    video:Play()
+    video.Ended:Wait()
+    video:Destroy()
+    -- End message
+    local label = Instance.new("TextLabel")
+    label.TextScaled = true
+    label.Size = UDim2.new(1, 0, 1, 0)
+    label.BackgroundTransparency = 1
+    label.Text = "Reload the script when ready.\nRemember it only works IF someone is holding a pet!"
+     local uist = Instance.new("UIStroke")
+    uist.Parent = video
+    uist.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+    uist.Thickness = 2.5
+    uist.Transparency = 0.25
+    uist.Color = Color3.new(0.7,0.7,0.7)
+    label.Parent = gui
+
+    task.wait(5)
+    gui:Destroy()
 end
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "How to use",
+    Text = "Click 'Play' to watch the tutorial in game!",
+    Duration = 10,
+    Button1 = "Play",
+    Callback = playTutorialBindable
+})
+
