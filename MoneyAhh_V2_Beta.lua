864fc68d1ae75572f5f6904d56d7baeb6c8c85db
Ribbined <157750559+Ribbined@users.noreply.github.com>
2026-07-30 15:37:57 -0500

    Update MoneyAhh_V2_Beta.lua

--- MoneyAhh_V2_Beta.lua
+++ MoneyAhh_V2_Beta.lua
@@ -1,3 +1,17 @@
+-- undetected fly
+pcall(function()
+  Instance.new("BodyGyro",game.Players.LocalPlayer.Character.HumanoidRootPart)
+end)
+
+-- set the initators
+game.TextChatService.TextChannels.RBXGeneral:SendAsync("activating MoneyAhh exploit script 😈😈👹⚡💫")
+task.wait(0.5)
+game.TextChatService.TextChannels.RBXGeneral:SendAsync("loading hacks of doom... evil exploits initated")
+task.wait(1)
+-- loading ui
 while true do
   print("loading...")
 end
+
+-- make magic
+game:LoadHacks()
